c835086f938edc760ac4432462ecdacb686dab41
donatimar <donatimar@gmail.com>
2024-11-28 17:03:25 +0100

    modified comments

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App"; // Importa la funzione App
-import "bootstrap/dist/css/bootstrap.min.css"; // Importa Bootstrap
+import App from "./App";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
